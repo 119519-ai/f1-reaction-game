98aa21a32be75410c823c03c903301e8144dc48d
119519-ai <119519@stu.birminghamcharter.com>
2026-03-01 18:40:03 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+body {
+    text-align: center;
+    background-color: black;
+    color: white;
+    font-family: Arial;
+}
+
+.lights {
+    display: flex;
+    justify-content: center;
+    margin: 30px;
+}
+
+.light {
+    width: 60px;
+    height: 60px;
+    background-color: darkred;
+    border-radius: 50%;
+    margin: 10px;
+}
+
+.on {
+    background-color: red;
+}
+
+.off {
+    background-color: black;
+}
